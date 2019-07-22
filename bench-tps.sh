ab80eacf9a6b6bfb0575a3119235b47e5a1dad03
Michael Vines <mvines@gmail.com>
2019-07-22 15:21:54 -0700

    Add bench-tps.sh

---
+++ bench-tps.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -ex
+cd "$(dirname "$0")"
+
+if [[ ! -f mint-keypair.json ]]; then
+  scp -o "ConnectTimeout=20" -o "BatchMode=yes" \
+    -o "StrictHostKeyChecking=no" -o "UserKnownHostsFile=/dev/null" \
+    solana@tds.solana.com:solana/config-local/mint-keypair.json .
+fi
+
+solana-wallet -u http://tds.solana.com:8899 -k mint-keypair.json balance
+
+if [[ ! -f bench-tps.json ]]; then
+  solana-keygen new -o bench-tps.json
+fi
+
+solana-wallet -u http://tds.solana.com:8899 -k mint-keypair.json \
+  pay "$(solana-keygen pubkey bench-tps.json)" 100000000
+solana-wallet -u http://tds.solana.com:8899 -k bench-tps.json balance
+
+export RUST_LOG=solana=info
+solana-bench-tps -n tds.solana.com:8001 -i bench-tps.json -N 2 --tx_count=1000 --thread-batch-sleep-ms=1000
