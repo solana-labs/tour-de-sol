8182f16f5a152bca983d6231a228b485de6afb80
Michael Vines <mvines@gmail.com>
2019-07-22 23:40:00 -0700

    Update rpc-check.sh

--- rpc-check.sh
+++ rpc-check.sh
@@ -19,6 +19,6 @@ curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "m
 curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getEpochVoteAccounts"}' $url | jq
 curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getEpochInfo"}' $url | jq
 curl -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getClusterNodes"}' $url | jq
-if [[ -f validator-keypair.json ]]; then
+if [[ -f ~/validator-keypair.json ]]; then
   solana-wallet --keypair ~/validator-keypair.json --url $url balance
 fi
