8222357f9058836a954bba482bf1598c5158afc8
Michael Vines <mvines@gmail.com>
2019-08-20 07:27:16 -0700

    Add edge shortcut

--- get-url.sh
+++ get-url.sh
@@ -1,6 +1,8 @@
 url=http://tds.solana.com:8899
 if [[ $1 = local ]]; then
   url=http://localhost:8899
+elif [[ $1 = edge ]]; then
+  url=http://edge.testnet.solana.com:8899
 elif [[ -n $1 ]]; then
   url=$1
 fi
