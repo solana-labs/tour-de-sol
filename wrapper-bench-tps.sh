2ecbe0fe5a4e42985967a2448f1727e42951b149
Justin Starry <justin@solana.com>
2019-10-23 12:44:20 -0400

    combine bench-tps.sh

--- wrapper-bench-tps.sh
+++ wrapper-bench-tps.sh
@@ -11,6 +11,5 @@ txCount=$3
 eval "$("$netDir/gce.sh" info --eval)"
 clientIp="NET_CLIENT${clientId}_IP"
 
-"$netDir/scp.sh" $tdsDir/remote-bench-tps.sh solana@${!clientIp}:.
-"$netDir/ssh.sh" ${!clientIp} killall solana-bench-tps || true
-"$netDir/ssh.sh" ${!clientIp} ./remote-bench-tps.sh $NET_VALIDATOR0_IP $txCount
+"$netDir/scp.sh" $tdsDir/bench-tps.sh solana@${!clientIp}:.
+"$netDir/ssh.sh" ${!clientIp} ./bench-tps.sh $NET_VALIDATOR0_IP $txCount remote
