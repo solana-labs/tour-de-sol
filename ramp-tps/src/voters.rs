fc1ae7e33ef5f8c8f29e99218e3bedf484c6be87
Michael Vines <mvines@gmail.com>
2019-10-25 20:32:34 -0700

    Authorize the mint_keypair to manipulate stake

--- ramp-tps/src/voters.rs
+++ ramp-tps/src/voters.rs
@@ -1,3 +1,4 @@
+use log::*;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::native_token::sol_to_lamports;
 use solana_sdk::pubkey::Pubkey;
@@ -6,7 +7,6 @@ use solana_sdk::transaction::Transaction;
 use solana_stake_api::stake_instruction;
 use solana_stake_api::stake_state::Authorized as StakeAuthorized;
 use std::str::FromStr;
-use log::*;
 
 pub fn fetch_remaining_voters(rpc_client: &RpcClient) -> Vec<Pubkey> {
     rpc_client
@@ -33,7 +33,7 @@ pub fn award_stake(
                 &mint_keypair.pubkey(),
                 &stake_account_keypair.pubkey(),
                 &vote_account_pubkey,
-                &StakeAuthorized::auto(&stake_account_keypair.pubkey()),
+                &StakeAuthorized::auto(&mint_keypair.pubkey()),
                 sol_to_lamports(sol_gift as f64),
             ),
             recent_blockhash,
@@ -41,8 +41,12 @@ pub fn award_stake(
 
         info!("Delegating {} SOL to {}", sol_gift, vote_account_pubkey);
         if let Err(err) = rpc_client
-            .send_and_confirm_transaction(&mut transaction, &[mint_keypair, &stake_account_keypair]) {
-                error!("Failed to delgate {} SOL to {}: {}", sol_gift, vote_account_pubkey, err);
+            .send_and_confirm_transaction(&mut transaction, &[mint_keypair, &stake_account_keypair])
+        {
+            error!(
+                "Failed to delgate {} SOL to {}: {}",
+                sol_gift, vote_account_pubkey, err
+            );
         }
     }
 }
